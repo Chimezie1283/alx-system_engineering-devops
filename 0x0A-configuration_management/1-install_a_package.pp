a3dba6db96a0d1ce85a8261ed350ba845c06f1d8
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-12-22 06:13:49 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
